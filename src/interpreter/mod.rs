7abf7d7b2611e36f5447e8b1c5eaf2903e69cc91
ericthelemur <owenconnors1@gmail.com>
2022-08-12 18:03:31 +0100

    Task 2.4: Execute

--- src/interpreter/mod.rs
+++ src/interpreter/mod.rs
@@ -36,11 +36,45 @@ impl ChipState {
         dbg!(&instruction);
         instruction
     }
+
+    //break a u16 into its nibbles
+    fn nibbles(n: u16) -> (u8, u8, u8, u8) {
+        let n3 = (n >> 12) as u8;
+        let n2 = ((n >> 8) & 0b1111) as u8;
+        let n1 = ((n >> 4) & 0b1111) as u8;
+        let n0 = (n & 0b1111) as u8;
+        // Just for debug checking
+        let nibbles = format!("{:#X} {:#X} {:#X} {:#X}", n3, n2, n1, n0);
+        dbg!(nibbles);
+
+        (n3, n2, n1, n0)
+    }
+
+    fn execute(&mut self, instruction: u16) {
+        let nbs = Self::nibbles(instruction);
+        match nbs {
+            // 0000 NOP: Nothing
+            (0x0, 0x0, 0x0, 0x0) => (),
+            // 00EE RET: Return from subroutine
+            (0x0, 0x0, 0xE, 0xE) => {
+                self.program_counter = self.stack[self.stack_pointer as usize];
+                self.stack_pointer -= 1;
+            },
+            // 8xy2 AND Vx, Vy: Set Vx = Vx AND Vy.
+            (0x8, x, y, 0x2) => {
+                self.registers[x as usize] &= self.registers[y as usize];
+            }
+            _ => panic!("Instruction either doesn't exist or hasn't been implemented yet"),
+        }
+    }
+
+    
 }
 
 impl chip8_base::Interpreter for ChipState {
     fn step(&mut self, keys: &chip8_base::Keys) -> Option<chip8_base::Display> {
         let instr = self.fetch();
+        self.execute(instr);
         return Some(self.display);
     }
 
