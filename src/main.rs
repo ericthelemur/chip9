c5367c37aaebdfbf2bb468cc48293afe9ca180c4
ericthelemur <owenconnors1@gmail.com>
2022-10-06 18:18:31 +0100

    Bonus: CLI Interface

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,35 @@
 use interpreter::ChipState;
+use clap::Parser;
 
 mod interpreter;
 
 fn main() {
     env_logger::init();
 
-    let vm = ChipState::new(700)
-        .load("roms/uwcs.ch8").unwrap();
+    let cli = Cli::parse();
+
+    let vm = ChipState::new(cli.freq)
+        .load(&cli.rom).unwrap();
 
     chip8_base::run(vm);
+}
+
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+struct Cli {
+    /// A CHIP-8 ROM to load into the interpreter
+    #[clap(validator = rom_exists)]
+    rom: String,
+    // Frequency to run the interpreter at
+    #[clap(action, default_value_t = 700)]
+    freq: u32,
+}
+
+fn rom_exists(f: &str) -> Result<(), &'static str> {
+    let p = std::path::Path::new(f);
+    if !p.is_file() {
+        Err("File does not exist.")
+    } else {
+        Ok(())
+    }
 }
