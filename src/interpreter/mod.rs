540806e04a518058e5612ff945a5ec54958234a5
ericthelemur <owenconnors1@gmail.com>
2022-10-06 17:41:47 +0100

    Task 1.7: Timings

--- src/interpreter/mod.rs
+++ src/interpreter/mod.rs
@@ -7,11 +7,12 @@ pub struct ChipState {
     display: chip8_base::Display,
     stack_pointer: u8,
     stack: [u16; 16],
+    speed: Duration,
     // ... there will be more
 }
 
 impl ChipState {
-    pub fn new() -> Self {
+    pub fn new(clock_freq: u32) -> Self {
         Self {
             memory: [0; 4096],
             registers: [0; 16],
@@ -19,6 +20,7 @@ impl ChipState {
             display: [[chip8_base::Pixel::default(); 64]; 32],
             stack_pointer: 0,
             stack: [0; 16],
+            speed: Duration::from_secs_f64(1_f64 / clock_freq as f64),
         }
     }
 }
@@ -29,7 +31,7 @@ impl chip8_base::Interpreter for ChipState {
     }
 
     fn speed(&self) -> Duration {
-        Duration::from_secs(1)
+        self.speed
     }
 
     fn buzzer_active(&self) -> bool {
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use interpreter::ChipState;
 mod interpreter;
 
 fn main() {
-    let vm = ChipState::new();
+    let vm = ChipState::new(700);
 
     chip8_base::run(vm);
 }
